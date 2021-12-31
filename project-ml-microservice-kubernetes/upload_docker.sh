abce0824838712b708100d43f2208f29b268c355
Deepesh Bhatia <db@gmail.com>
2021-12-31 16:52:43 +0530

    project4

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=dbhatia86/dockerproj
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login 
+docker image tag dockerproj $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
